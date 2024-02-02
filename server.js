c3479df9e733e142494a25f07aab285e1483bde6
yeetyeet1116 <106297784+yahyaaman@users.noreply.github.com>
2024-02-02 11:21:58 +0500

    .

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 import { app } from "./app.js";
 import { connectDB } from "./data/database.js";
 
-connectDB();
-
 app.listen(process.env.port, (err) => {
   if (!err) {
+    connectDB();
     console.log(`Server running on port ${process.env.port}`);
   } else {
     console.log(err);
