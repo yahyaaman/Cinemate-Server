88f871ced85a9ee3cd32992eb3dc9e1c63f4d93f
yeetyeet1116 <106297784+yahyaaman@users.noreply.github.com>
2024-02-04 16:42:01 +0500

    .

---
+++ models/collection.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const collectionSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  image: {
+    type: String,
+  },
+  items: [
+    {
+      type: Object,
+      required: true,
+    },
+  ],
+  user: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+    required: false,
+  },
+});
+
+const Collection = mongoose.model("Collection", collectionSchema);
+
+export default Collection;
